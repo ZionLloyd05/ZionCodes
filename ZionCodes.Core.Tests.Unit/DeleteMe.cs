a7e3f80a732b188939d41336d3bc2851ee96301f
Alagbala Damilola <zionlloyd05@gmail.com>
2021-05-02 19:34:00 +0100

    INFRA: Initialize new unit test project

---
+++ ZionCodes.Core.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,12 @@
+using FluentAssertions;
+using Xunit;
+
+namespace ZionCodes.Core.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() =>
+            true.Should().BeTrue();
+    }
+}
