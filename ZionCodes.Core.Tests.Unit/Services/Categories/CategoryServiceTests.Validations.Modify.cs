02b55ecf2355118c21f130e39eb192d324f8bbcf
Alagbala Damilola <zionlloyd05@gmail.com>
2021-06-25 18:38:09 +0100

    ShouldThrowValidationExceptionOnModifyWhenIdIsInvalidAndLogItAsync -> FAIL

--- ZionCodes.Core.Tests.Unit/Services/Categories/CategoryServiceTests.Validations.Modify.cs
+++ ZionCodes.Core.Tests.Unit/Services/Categories/CategoryServiceTests.Validations.Modify.cs
@@ -43,5 +43,43 @@ namespace ZionCodes.Core.Tests.Unit.Services.Categories
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnModifyWhenIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid invalidFeeId = Guid.Empty;
+            DateTimeOffset dateTime = GetRandomDateTime();
+            Category randomCategory = CreateRandomCategory(dateTime);
+            Category inputCategory = randomCategory;
+            inputCategory.Id = default;
+
+            var invalidCategoryInputException = new InvalidCategoryException(
+                parameterName: nameof(Category.Id),
+                parameterValue: inputCategory.Id);
+
+            var expectedCategoryValidationException =
+                new CategoryValidationException(invalidCategoryInputException);
+
+            // when
+            ValueTask<Category> modifyCategoryTask =
+                this.categoryService.ModifyCategoryAsync(inputCategory);
+
+            // then
+            await Assert.ThrowsAsync<CategoryValidationException>(() =>
+                modifyCategoryTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedCategoryValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectCategoryByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
