80bc3eb778c11db6eb157c1b1e279d52ae9e0b6f
Alagbala Damilola <zionlloyd05@gmail.com>
2021-06-26 13:53:48 +0100

    Tag model setup and migrations

---
+++ ZionCodes.Core/Brokers/Storages/IStorageBroker.Tag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ZionCodes.Core.Models.Categories;
+using ZionCodes.Core.Models.Tags;
+
+namespace ZionCodes.Core.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        public ValueTask<Tag> InsertTagAsync(Tag tag);
+        public IQueryable<Tag> SelectAllTags();
+        public ValueTask<Tag> SelectTagByIdAsync(Guid tagId);
+        public ValueTask<Tag> UpdateTagAsync(Tag tag);
+        public ValueTask<Tag> DeleteTagAsync(Tag tag);
+    }
+}
---
+++ ZionCodes.Core/Brokers/Storages/StorageBroker.Tag.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using ZionCodes.Core.Models.Categories;
+
+namespace ZionCodes.Core.Brokers.Storages
+{
+    public partial class StorageBroker : IStorageBroker
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public async ValueTask<Category> InsertCategoryAsync(Category category)
+        {
+            EntityEntry<Category> categoryEntityEntry = await this.Categories.AddAsync(category);
+            await this.SaveChangesAsync();
+
+            return categoryEntityEntry.Entity;
+        }
+
+        public IQueryable<Category> SelectAllCategories() => this.Categories.AsQueryable();
+
+        public async ValueTask<Category> SelectCategoryByIdAsync(Guid categoryId)
+        {
+            this.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+
+            return await Categories.FindAsync(categoryId);
+        }
+
+        public async ValueTask<Category> UpdateCategoryAsync(Category category)
+        {
+            EntityEntry<Category> categoryEntityEntry = this.Categories.Update(category);
+            await this.SaveChangesAsync();
+
+            return categoryEntityEntry.Entity;
+        }
+
+        public async ValueTask<Category> DeleteCategoryAsync(Category category)
+        {
+            EntityEntry<Category> categoryEntityEntry = this.Categories.Remove(category);
+            await this.SaveChangesAsync();
+
+            return categoryEntityEntry.Entity;
+        }
+
+    }
+}
