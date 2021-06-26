074cb610cd0e2000069ddd2760507383150f54c2
Alagbala Damilola <zionlloyd05@gmail.com>
2021-06-26 03:41:39 +0100

    Category acceptance tests

---
+++ ZionCodes.Core.Tests.Acceptance/APIs/Categories/CategoriesApiTests.Logic.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using RESTFulSense.Exceptions;
+using Xunit;
+using ZionCodes.Core.Tests.Acceptance.Models.Categories;
+
+namespace ZionCodes.Core.Tests.Acceptance.APIs.Categories
+{
+    public partial class CategoriesApiTests
+    {
+        [Fact]
+        public async Task ShouldPostCategoryAsync()
+        {
+            // given
+            Category randomCategory = CreateRandomCategory();
+            Category inputCategory = randomCategory;
+            Category expectedCategory = inputCategory;
+
+            // when 
+            await this.apiBroker.PostCategoryAsync(inputCategory);
+
+            Category actualCategory =
+                 await this.apiBroker.GetCategoryByIdAsync(inputCategory.Id);
+
+            // then
+            actualCategory.Should().BeEquivalentTo(expectedCategory);
+            await this.apiBroker.DeleteCategoryByIdAsync(actualCategory.Id);
+        }
+
+        [Fact]
+        public async Task ShouldPutCategoryAsync()
+        {
+            // given
+            Category randomCategory = await PostRandomCategoryAsync();
+            Category modifiedCategory = UpdateCategoryRandom(randomCategory);
+
+            // when
+            await this.apiBroker.PutCategoryAsync(modifiedCategory);
+
+            Category actualCategory =
+                await this.apiBroker.GetCategoryByIdAsync(randomCategory.Id);
+
+            // then
+            actualCategory.Should().BeEquivalentTo(modifiedCategory);
+            await this.apiBroker.DeleteCategoryByIdAsync(actualCategory.Id);
+        }
+
+        [Fact]
+        public async Task ShouldGetAllCategoriesAsync()
+        {
+            //given
+            var randomCategories = new List<Category>();
+
+            for (var i = 0; i <= GetRandomNumber(); i++)
+            {
+                randomCategories.Add(await PostRandomCategoryAsync());
+            }
+
+            List<Category> inputedCategories = randomCategories;
+            List<Category> expectedCategories = inputedCategories.ToList();
+
+            //when 
+            List<Category> actualCategories = await this.apiBroker.GetAllCategoriesAsync();
+
+            //then
+            foreach (var expectedcalendar in expectedCategories)
+            {
+                Category actualCategory = actualCategories.Single(calendar => calendar.Id == expectedcalendar.Id);
+
+                actualCategory.Should().BeEquivalentTo(expectedcalendar);
+                await this.apiBroker.DeleteCategoryByIdAsync(actualCategory.Id);
+            }
+        }
+
+        [Fact]
+        public async Task ShouldDeleteCategoryAsync()
+        {
+            //given
+            Category randomCategory = await PostRandomCategoryAsync();
+            Category inputCategory = randomCategory;
+            Category expectedCategory = inputCategory;
+
+            //when
+            Category deletedCategory =
+                await this.apiBroker.DeleteCategoryByIdAsync(inputCategory.Id);
+
+            ValueTask<Category> getCategoryByIdTask =
+                this.apiBroker.DeleteCategoryByIdAsync(inputCategory.Id);
+
+            // then
+            deletedCategory.Should().BeEquivalentTo(expectedCategory);
+
+            await Assert.ThrowsAsync<HttpResponseNotFoundException>(() =>
+               getCategoryByIdTask.AsTask());
+        }
+    }
+}
---
+++ ZionCodes.Core.Tests.Acceptance/APIs/Categories/CategoriesApiTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tynamix.ObjectFiller;
+using Xunit;
+using ZionCodes.Core.Tests.Acceptance.Brokers;
+using ZionCodes.Core.Tests.Acceptance.Models.Categories;
+
+namespace ZionCodes.Core.Tests.Acceptance.APIs.Categories
+{
+    [Collection(nameof(ApiTestCollection))]
+    public partial class CategoriesApiTests
+    {
+        private readonly ApiBroker apiBroker;
+
+        public CategoriesApiTests(ApiBroker categoryApiBroker) =>
+            this.apiBroker = categoryApiBroker;
+
+        private static Category CreateRandomCategory() =>
+             CreateRandomCategoryFiller().Create();
+
+        private async ValueTask<Category> PostRandomCategoryAsync()
+        {
+            Category randomCategory = CreateRandomCategory();
+            await this.apiBroker.PostCategoryAsync(randomCategory);
+
+            return randomCategory;
+        }
+
+        private static Category UpdateCategoryRandom(Category category)
+        {
+            DateTimeOffset now = DateTimeOffset.UtcNow;
+            var filler = new Filler<Category>();
+
+            filler.Setup()
+                .OnProperty(category => category.Id).Use(category.Id)
+                .OnProperty(category => category.CreatedBy).Use(category.CreatedBy)
+                .OnProperty(category => category.UpdatedBy).Use(category.UpdatedBy)
+                .OnProperty(category => category.CreatedDate).Use(category.CreatedDate)
+                .OnProperty(category => category.UpdatedDate).Use(now)
+                .OnType<DateTimeOffset>().Use(GetRandomDateTime());
+
+            return filler.Create();
+        }
+
+        private static Filler<Category> CreateRandomCategoryFiller()
+        {
+            DateTimeOffset now = DateTimeOffset.UtcNow;
+            Guid posterId = Guid.NewGuid();
+
+            var filler = new Filler<Category>();
+
+            filler.Setup()
+                .OnProperty(category => category.CreatedBy).Use(posterId)
+                .OnProperty(category => category.UpdatedBy).Use(posterId)
+                .OnProperty(category => category.CreatedDate).Use(now)
+                .OnProperty(category => category.UpdatedDate).Use(now)
+                .OnType<DateTimeOffset>().Use(GetRandomDateTime());
+
+            return filler;
+        }
+
+        private static DateTimeOffset GetRandomDateTime() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static int GetRandomNumber() => new IntRange(min: 2, max: 10).GetValue();
+    }
+}
