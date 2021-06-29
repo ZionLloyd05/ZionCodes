353bd247f6d62530cd09154c7955969baf8bb029
Alagbala Damilola <zionlloyd05@gmail.com>
2021-06-29 20:35:38 +0100

    ShouldThrowValidationExceptionOnRetrieveByIdWhenStorageTagIsNullAndLogItAsync -> FAIL

--- ZionCodes.Core.Tests.Unit/Services/Tags/TagServiceTests.Validations.RetrieveById.cs
+++ ZionCodes.Core.Tests.Unit/Services/Tags/TagServiceTests.Validations.RetrieveById.cs
@@ -50,5 +50,46 @@ namespace ZionCodes.Core.Tests.Unit.Services.Tags
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnRetrieveByIdWhenStorageTagIsNullAndLogItAsync()
+        {
+            // given
+            Guid randomTagId = Guid.NewGuid();
+            Guid someTagId = randomTagId;
+            Tag invalidStorageTag = null;
+            var notFoundTagException = new NotFoundTagException(someTagId);
+
+            var exceptionTagValidationException =
+                new TagValidationException(notFoundTagException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectTagByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(invalidStorageTag);
+
+            // when
+            ValueTask<Tag> retrieveTagByIdTask =
+                this.tagService.RetrieveTagByIdAsync(someTagId);
+
+            // then
+            await Assert.ThrowsAsync<TagValidationException>(() =>
+                retrieveTagByIdTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(exceptionTagValidationException))),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectTagByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
---
+++ ZionCodes.Core/Models/Tags/Exceptions/NotFoundTagException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ZionCodes.Core.Models.Tags.Exceptions
+{
+    public class NotFoundTagException : Exception
+    {
+        public NotFoundTagException(Guid tagId)
+           : base($"Couldn't find tag with Id: {tagId}.") { }
+    }
+}
