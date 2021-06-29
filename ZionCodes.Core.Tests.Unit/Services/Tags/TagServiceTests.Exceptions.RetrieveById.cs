22a5a9a01c245bb55a533b1c044a40dec199bf66
Alagbala Damilola <zionlloyd05@gmail.com>
2021-06-29 20:47:56 +0100

    ShouldThrowDependencyExceptionOnRetrieveByIdWhenSqlExceptionOccursAndLogItAsync -> PASS

---
+++ ZionCodes.Core.Tests.Unit/Services/Tags/TagServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Xunit;
+using ZionCodes.Core.Models.Tags;
+using ZionCodes.Core.Models.Tags.Exceptions;
+
+namespace ZionCodes.Core.Tests.Unit.Services.Tags
+{
+    public partial class TagServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRetrieveByIdWhenSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid randomTagId = Guid.NewGuid();
+            Guid inputTagId = randomTagId;
+            SqlException sqlException = GetSqlException();
+
+            var exceptionTagDependencyException =
+                new TagDependencyException(sqlException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectTagByIdAsync(inputTagId))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Tag> retrieveTagByIdTask =
+                this.tagService.RetrieveTagByIdAsync(inputTagId);
+
+            // then
+            await Assert.ThrowsAsync<TagDependencyException>(() =>
+                retrieveTagByIdTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(exceptionTagDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectTagByIdAsync(inputTagId),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
