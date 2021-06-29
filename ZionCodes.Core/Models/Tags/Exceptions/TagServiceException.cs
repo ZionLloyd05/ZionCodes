ca710292c97f1d392a8c188188a25532fe7cb2e0
Alagbala Damilola <zionlloyd05@gmail.com>
2021-06-29 16:53:52 +0100

    ShouldThrowServiceExceptionOnAddWhenExceptionOccursAndLogItAsync -> FAIL

---
+++ ZionCodes.Core/Models/Tags/Exceptions/TagServiceException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ZionCodes.Core.Models.Tags.Exceptions
+{
+    public class TagServiceException : Exception
+    {
+        public TagServiceException(Exception innerException)
+            : base("Service error occurred, contact support.", innerException) { }
+    }
+}
