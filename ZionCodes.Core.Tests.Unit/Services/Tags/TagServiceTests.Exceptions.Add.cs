cedea6d455e93b9093a63ab1c2b442f2d20897f6
Alagbala Damilola <zionlloyd05@gmail.com>
2021-06-29 16:47:03 +0100

    ShouldThrowDependencyExceptionOnAddWhenDbExceptionOccursAndLogItAsync -> FAIL

--- ZionCodes.Core.Tests.Unit/Services/Tags/TagServiceTests.Exceptions.Add.cs
+++ ZionCodes.Core.Tests.Unit/Services/Tags/TagServiceTests.Exceptions.Add.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Xunit;
 using ZionCodes.Core.Models.Tags;
@@ -43,6 +44,52 @@ namespace ZionCodes.Core.Tests.Unit.Services.Tags
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(It.Is(SameExceptionAs(expectedTagDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertTagAsync(inputTag),
+                    Times.Once);
+
+            //this.dateTimeBrokerMock.Verify(broker =>
+            //    broker.GetCurrentDateTime(),
+            //        Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnAddWhenDbExceptionOccursAndLogItAsync()
+        {
+            // given
+            DateTimeOffset dateTime = GetRandomDateTime();
+            Tag randomTag = CreateRandomTag(dateTime);
+            Tag inputTag = randomTag;
+            inputTag.UpdatedBy = inputTag.CreatedBy;
+            var databaseUpdateException = new DbUpdateException();
+
+            var expectedTagDependencyException =
+                new TagDependencyException(databaseUpdateException);
+
+            //this.dateTimeBrokerMock.Setup(broker =>
+            //    broker.GetCurrentDateTime())
+            //        .Returns(dateTime);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertTagAsync(inputTag))
+                    .ThrowsAsync(databaseUpdateException);
+
+            // when
+            ValueTask<Tag> createTagTask =
+                this.tagService.AddTagAsync(inputTag);
+
+            // then
+            await Assert.ThrowsAsync<TagDependencyException>(() =>
+                createTagTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedTagDependencyException))),
                     Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
