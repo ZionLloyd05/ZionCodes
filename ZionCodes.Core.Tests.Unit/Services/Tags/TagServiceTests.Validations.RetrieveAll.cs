f2115e8827303669dadb193538dbf437c0ddeeb0
Alagbala Damilola <zionlloyd05@gmail.com>
2021-06-30 11:47:18 +0100

    ShouldLogWarningOnRetrieveAllWhenTagsWasEmptyAndLogIt -> FAIL

---
+++ ZionCodes.Core.Tests.Unit/Services/Tags/TagServiceTests.Validations.RetrieveAll.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Xunit;
+using ZionCodes.Core.Models.Tags;
+
+namespace ZionCodes.Core.Tests.Unit.Services.Tags
+{
+    public partial class TagServiceTests
+    {
+        [Fact]
+        public void ShouldLogWarningOnRetrieveAllWhenTagsWasEmptyAndLogIt()
+        {
+            // given
+            IQueryable<Tag> emptyStorageTags = new List<Tag>().AsQueryable();
+            IQueryable<Tag> expectedTags = emptyStorageTags;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllTags())
+                    .Returns(expectedTags);
+
+            // when
+            IQueryable<Tag> actualTag =
+                this.tagService.RetrieveAllTags();
+
+            // then
+            actualTag.Should().BeEquivalentTo(expectedTags);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogWarning("No categories found in storage."));
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
