2a022fd6321c57300f0b1127674057111413de11
Alagbala Damilola <zionlloyd05@gmail.com>
2021-06-30 11:17:00 +0100

    ShouldThrowDependencyExceptionOnRetrieveAllTagsWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ ZionCodes.Core.Tests.Unit/Services/Tags/TagServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+using ZionCodes.Core.Models.Tags.Exceptions;
+
+namespace ZionCodes.Core.Tests.Unit.Services.Tags
+{
+    public partial class TagServiceTests
+    {
+        [Fact]
+        public void ShouldThrowDependencyExceptionOnRetrieveAllTagsWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            var sqlException = GetSqlException();
+
+            var expectedTagDependencyException =
+                new TagDependencyException(sqlException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllCategories())
+                    .Throws(sqlException);
+
+            // when . then
+            Assert.Throws<TagDependencyException>(() =>
+                this.tagService.RetrieveAllTags());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllCategories(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(expectedTagDependencyException))),
+                    Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
