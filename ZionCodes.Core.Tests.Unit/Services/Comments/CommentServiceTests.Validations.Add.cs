80ebe2f9494c25bb46e9a095eba9d933d068dc7c
Alagbala Damilola <zionlloyd05@gmail.com>
2021-07-07 19:57:23 +0100

    ShouldThrowValidationExceptionOnCreateWhenCommentIdIsInvalidAndLogItAsync -> FAIL

--- ZionCodes.Core.Tests.Unit/Services/Comments/CommentServiceTests.Validations.Add.cs
+++ ZionCodes.Core.Tests.Unit/Services/Comments/CommentServiceTests.Validations.Add.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Moq;
 using Xunit;
 using ZionCodes.Core.Models.Comments;
@@ -39,5 +40,43 @@ namespace ZionCodes.Core.Tests.Unit.Services.Comments
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnCreateWhenCommentIdIsInvalidAndLogItAsync()
+        {
+            //given
+            DateTimeOffset dateTime = GetRandomDateTime();
+            Comment randomComment = CreateRandomComment(dateTime);
+            Comment inputComment = randomComment;
+            inputComment.Id = default;
+
+            var invalidCommentInputException = new InvalidCommentException(
+                parameterName: nameof(Comment.Id),
+                parameterValue: inputComment.Id);
+
+            var expectedCommentValidationException =
+                new CommentValidationException(invalidCommentInputException);
+
+            // when
+            ValueTask<Comment> registerCommentTask =
+                this.commentService.AddCommentAsync(inputComment);
+
+            // then
+            await Assert.ThrowsAsync<CommentValidationException>(() =>
+                registerCommentTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedCommentValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertCommentAsync(It.IsAny<Comment>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
     }
 }
---
+++ ZionCodes.Core/Models/Comments/Exeptions/InvalidCommentException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ZionCodes.Core.Models.Comments.Exceptions
+{
+    public class InvalidCommentException : Exception
+    {
+        public InvalidCommentException(string parameterName, object parameterValue)
+           : base($"Invalid Comment, " +
+                  $"ParameterName: {parameterName}, " +
+                  $"ParameterValue: {parameterValue}")
+        { }
+    }
+}
