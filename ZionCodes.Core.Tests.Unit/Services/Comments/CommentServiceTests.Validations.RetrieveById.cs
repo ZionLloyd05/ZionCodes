2d9f7c47a5d5d42a16580c3f1b97376a7c1dd4de
Alagbala Damilola <zionlloyd05@gmail.com>
2021-07-08 18:49:45 +0100

    ShouldThrowValidationExceptionOnRetrieveByIdWhenIdIsInvalidAndLogItAsync -> FAIL

---
+++ ZionCodes.Core.Tests.Unit/Services/Comments/CommentServiceTests.Validations.RetrieveById.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+using ZionCodes.Core.Models.Comments;
+using ZionCodes.Core.Models.Comments.Exceptions;
+
+namespace ZionCodes.Core.Tests.Unit.Services.Comments
+{
+    public partial class CommentServiceTests
+    {
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnRetrieveByIdWhenIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid randomCommentId = default;
+            Guid inputCommentId = randomCommentId;
+
+            var invalidCommentInputException = new InvalidCommentInputException(
+                    parameterName: nameof(Comment.Id),
+                    parameterValue: inputCommentId);
+
+            var expectedCommentValidationException =
+                new CommentValidationException(invalidCommentInputException);
+
+            // when
+            ValueTask<Comment> retrieveCommentByIdTask =
+                this.commentService.RetrieveCommentByIdAsync(inputCommentId);
+
+            // then
+            await Assert.ThrowsAsync<CommentValidationException>(() =>
+                retrieveCommentByIdTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedCommentValidationException))),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectCommentByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
