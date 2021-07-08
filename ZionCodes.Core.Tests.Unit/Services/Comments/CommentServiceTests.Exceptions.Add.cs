ac876ca64193668d01f72bfe1b8905ff35b1d492
Alagbala Damilola <zionlloyd05@gmail.com>
2021-07-08 16:40:03 +0100

    ShouldThrowDependencyExceptionOnAddWhenSqlExceptionOccursAndLogItAsync -> FAIL

---
+++ ZionCodes.Core.Tests.Unit/Services/Comments/CommentServiceTests.Exceptions.Add.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+using ZionCodes.Core.Models.Comments;
+using ZionCodes.Core.Models.Comments.Exceptions;
+
+namespace ZionCodes.Core.Tests.Unit.Services.Comments
+{
+    public partial class CommentServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnAddWhenSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            DateTimeOffset dateTime = GetRandomDateTime();
+            Comment randomComment = CreateRandomComment(dateTime);
+            Comment inputComment = randomComment;
+            var sqlException = GetSqlException();
+
+            var expectedCommentDependencyException =
+                new CommentDependencyException(sqlException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime())
+                    .Returns(dateTime);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertCommentAsync(inputComment))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Comment> createCommentTask =
+                this.commentService.AddCommentAsync(inputComment);
+
+            // then
+            await Assert.ThrowsAsync<CommentDependencyException>(() =>
+                createCommentTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(expectedCommentDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertCommentAsync(inputComment),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
---
+++ ZionCodes.Core/Models/Comments/Exeptions/CommentDependencyException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ZionCodes.Core.Models.Comments.Exceptions
+{
+    public class CommentDependencyException : Exception
+    {
+        public CommentDependencyException(Exception innerException)
+           : base("Service dependency error occurred, category support.", innerException) { }
+    }
+}
