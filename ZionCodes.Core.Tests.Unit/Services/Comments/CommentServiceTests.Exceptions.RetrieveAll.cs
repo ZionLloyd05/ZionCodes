369932f0bc70a658b4fe7f39625472d80dbd8708
Alagbala Damilola <zionlloyd05@gmail.com>
2021-07-08 18:10:23 +0100

    ShouldThrowDependencyExceptionOnRetrieveAllCommentsWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ ZionCodes.Core.Tests.Unit/Services/Comments/CommentServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+using ZionCodes.Core.Models.Comments.Exceptions;
+
+namespace ZionCodes.Core.Tests.Unit.Services.Comments
+{
+    public partial class CommentServiceTests
+    {
+        [Fact]
+        public void ShouldThrowDependencyExceptionOnRetrieveAllCommentsWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            var sqlException = GetSqlException();
+
+            var expectedCommentDependencyException =
+                new CommentDependencyException(sqlException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllComments())
+                    .Throws(sqlException);
+
+            // when . then
+            Assert.Throws<CommentDependencyException>(() =>
+                this.commentService.RetrieveAllComments());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllComments(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(expectedCommentDependencyException))),
+                    Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
