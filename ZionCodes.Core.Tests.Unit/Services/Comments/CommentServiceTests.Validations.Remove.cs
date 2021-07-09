dbea3cfc5bdbe102683085874f7ba2e330b455ec
Alagbala Damilola <zionlloyd05@gmail.com>
2021-07-09 10:43:49 +0100

    ShouldThrowValidatonExceptionOnDeleteWhenStorageCommentIsInvalidAndLogItAsync -> FAIL

--- ZionCodes.Core.Tests.Unit/Services/Comments/CommentServiceTests.Validations.Remove.cs
+++ ZionCodes.Core.Tests.Unit/Services/Comments/CommentServiceTests.Validations.Remove.cs
@@ -50,5 +50,47 @@ namespace ZionCodes.Core.Tests.Unit.Services.Comments
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
 
+        [Fact]
+        public async Task ShouldThrowValidatonExceptionOnDeleteWhenStorageCommentIsInvalidAndLogItAsync()
+        {
+            // given
+            DateTimeOffset dateTime = GetRandomDateTime();
+            Comment randomComment = CreateRandomComment(dateTime);
+            Guid inputCommentId = randomComment.Id;
+            Comment inputComment = randomComment;
+            Comment nullStorageComment = null;
+
+            var notFoundCommentException = new NotFoundCommentException(inputCommentId);
+
+            var expectedCommentValidationException =
+                new CommentValidationException(notFoundCommentException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectCommentByIdAsync(inputCommentId))
+                    .ReturnsAsync(nullStorageComment);
+
+            // when
+            ValueTask<Comment> actualCommentTask =
+                this.commentService.RemoveCommentByIdAsync(inputCommentId);
+
+            // then
+            await Assert.ThrowsAsync<CommentValidationException>(() => actualCommentTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedCommentValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectCommentByIdAsync(inputCommentId),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteCommentAsync(It.IsAny<Comment>()),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
