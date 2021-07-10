fd591a282a9adaa26510ade8380de1689a64b39e
Alagbala Damilola <zionlloyd05@gmail.com>
2021-07-10 19:08:16 +0100

    ReadingNotes model and migration implemented

---
+++ ZionCodes.Core/Brokers/Storages/StorageBroker.ReadingNote.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using ZionCodes.Core.Models.ReadingNotes;
+
+namespace ZionCodes.Core.Brokers.Storages
+{
+    public partial class StorageBroker : IStorageBroker
+    {
+        public DbSet<ReadingNote> ReadingNotes { get; set; }
+
+        public async ValueTask<ReadingNote> InsertReadingNoteAsync(ReadingNote readingNote)
+        {
+            EntityEntry<ReadingNote> readingNoteEntityEntry = await this.ReadingNotes.AddAsync(readingNote);
+            await this.SaveChangesAsync();
+
+            return readingNoteEntityEntry.Entity;
+        }
+
+        public IQueryable<ReadingNote> SelectAllReadingNotes() => this.ReadingNotes.AsQueryable();
+
+        public async ValueTask<ReadingNote> SelectReadingNoteByIdAsync(Guid readingNoteId)
+        {
+            this.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+
+            return await ReadingNotes.FindAsync(readingNoteId);
+        }
+
+        public async ValueTask<ReadingNote> UpdateReadingNoteAsync(ReadingNote readingNote)
+        {
+            EntityEntry<ReadingNote> readingNoteEntityEntry = this.ReadingNotes.Update(readingNote);
+            await this.SaveChangesAsync();
+
+            return readingNoteEntityEntry.Entity;
+        }
+
+        public async ValueTask<ReadingNote> DeleteReadingNoteAsync(ReadingNote readingNote)
+        {
+            EntityEntry<ReadingNote> readingNoteEntityEntry = this.ReadingNotes.Remove(readingNote);
+            await this.SaveChangesAsync();
+
+            return readingNoteEntityEntry.Entity;
+        }
+    }
+}
