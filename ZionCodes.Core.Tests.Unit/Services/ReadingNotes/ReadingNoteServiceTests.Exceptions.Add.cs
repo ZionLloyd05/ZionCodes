eec583b9ebff72887a7a32f84a31baa0d55f8cee
Alagbala Damilola <zionlloyd05@gmail.com>
2021-07-13 14:17:38 +0100

    ShouldThrowDependencyExceptionOnAddWhenSqlExceptionOccursAndLogItAsync -> FAIL

---
+++ ZionCodes.Core.Tests.Unit/Services/ReadingNotes/ReadingNoteServiceTests.Exceptions.Add.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+using ZionCodes.Core.Models.ReadingNotes;
+using ZionCodes.Core.Models.ReadingNotes.Exceptions;
+
+namespace ZionCodes.Core.Tests.Unit.Services.ReadingNotes
+{
+    public partial class ReadingNoteServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnAddWhenSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            DateTimeOffset dateTime = GetRandomDateTime();
+            ReadingNote randomReadingNote = CreateRandomReadingNote(dateTime);
+            ReadingNote inputReadingNote = randomReadingNote;
+            inputReadingNote.UpdatedBy = inputReadingNote.CreatedBy;
+            var sqlException = GetSqlException();
+
+            var expectedReadingNoteDependencyException =
+                new ReadingNoteDependencyException(sqlException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime())
+                    .Returns(dateTime);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertReadingNoteAsync(inputReadingNote))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<ReadingNote> createReadingNoteTask =
+                this.readingNoteService.AddReadingNoteAsync(inputReadingNote);
+
+            // then
+            await Assert.ThrowsAsync<ReadingNoteDependencyException>(() =>
+                createReadingNoteTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(expectedReadingNoteDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertReadingNoteAsync(inputReadingNote),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
