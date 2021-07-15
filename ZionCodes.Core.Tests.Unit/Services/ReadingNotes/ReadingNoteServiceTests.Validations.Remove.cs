3cfeb34f064f1a2c982a4c2d0ffa97f171c1c97c
Alagbala Damilola <zionlloyd05@gmail.com>
2021-07-16 00:57:54 +0100

    ShouldThrowValidatonExceptionOnDeleteWhenStorageReadingNoteIsInvalidAndLogItAsync -> FAIL

--- ZionCodes.Core.Tests.Unit/Services/ReadingNotes/ReadingNoteServiceTests.Validations.Remove.cs
+++ ZionCodes.Core.Tests.Unit/Services/ReadingNotes/ReadingNoteServiceTests.Validations.Remove.cs
@@ -49,5 +49,48 @@ namespace ZionCodes.Core.Tests.Unit.Services.ReadingNotes
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowValidatonExceptionOnDeleteWhenStorageReadingNoteIsInvalidAndLogItAsync()
+        {
+            // given
+            DateTimeOffset dateTime = GetRandomDateTime();
+            ReadingNote randomReadingNote = CreateRandomReadingNote(dateTime);
+            Guid inputReadingNoteId = randomReadingNote.Id;
+            ReadingNote inputReadingNote = randomReadingNote;
+            ReadingNote nullStorageReadingNote = null;
+
+            var notFoundReadingNoteException = new NotFoundReadingNoteException(inputReadingNoteId);
+
+            var expectedReadingNoteValidationException =
+                new ReadingNoteValidationException(notFoundReadingNoteException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectReadingNoteByIdAsync(inputReadingNoteId))
+                    .ReturnsAsync(nullStorageReadingNote);
+
+            // when
+            ValueTask<ReadingNote> actualReadingNoteTask =
+                this.readingNoteService.RemoveReadingNoteByIdAsync(inputReadingNoteId);
+
+            // then
+            await Assert.ThrowsAsync<ReadingNoteValidationException>(() => actualReadingNoteTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedReadingNoteValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectReadingNoteByIdAsync(inputReadingNoteId),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteReadingNoteAsync(It.IsAny<ReadingNote>()),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
