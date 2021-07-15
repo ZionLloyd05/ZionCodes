5cc8fd463088fb52353003077c8e950bfc7a512a
Alagbala Damilola <zionlloyd05@gmail.com>
2021-07-16 00:21:09 +0100

    ShouldThrowValidatonExceptionOnDeleteWhenIdIsInvalidAndLogItAsync -> FAIL

---
+++ ZionCodes.Core.Tests.Unit/Services/ReadingNotes/ReadingNoteServiceTests.Validations.Remove.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+using ZionCodes.Core.Models.ReadingNotes;
+using ZionCodes.Core.Models.ReadingNotes.Exceptions;
+
+namespace ZionCodes.Core.Tests.Unit.Services.ReadingNotes
+{
+    public partial class ReadingNoteServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidatonExceptionOnDeleteWhenIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid randomReadingNoteId = default;
+            Guid inputReadingNoteId = randomReadingNoteId;
+
+            var invalidReadingNoteInputException = new InvalidReadingNoteException(
+                parameterName: nameof(ReadingNote.Id),
+                parameterValue: inputReadingNoteId);
+
+            var expectedReadingNoteValidationException =
+                new ReadingNoteValidationException(invalidReadingNoteInputException);
+
+            // when
+            ValueTask<ReadingNote> deleteReadingNoteTask =
+               this.readingNoteService.RemoveReadingNoteByIdAsync(inputReadingNoteId);
+
+            // then
+            await Assert.ThrowsAsync<ReadingNoteValidationException>(() => deleteReadingNoteTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedReadingNoteValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectReadingNoteByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteReadingNoteAsync(It.IsAny<ReadingNote>()),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
