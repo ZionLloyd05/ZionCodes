db5b8e443e84668485bfb083ae869de6221372f2
Alagbala Damilola <zionlloyd05@gmail.com>
2021-07-15 18:32:06 +0100

    ShouldThrowDependencyExceptionOnRetrieveAllReadingNotesWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ ZionCodes.Core.Tests.Unit/Services/ReadingNotes/ReadingNoteServiceTests.Exceptions.RemoveAll.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+using ZionCodes.Core.Models.ReadingNotes.Exceptions;
+
+namespace ZionCodes.Core.Tests.Unit.Services.ReadingNotes
+{
+    public partial class ReadingNoteServiceTests
+    {
+        [Fact]
+        public void ShouldThrowDependencyExceptionOnRetrieveAllReadingNotesWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            var sqlException = GetSqlException();
+
+            var expectedReadingNoteDependencyException =
+                new ReadingNoteDependencyException(sqlException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllReadingNotes())
+                    .Throws(sqlException);
+
+            // when . then
+            Assert.Throws<ReadingNoteDependencyException>(() =>
+                this.readingNoteService.RetrieveAllReadingNotes());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllReadingNotes(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(expectedReadingNoteDependencyException))),
+                    Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
