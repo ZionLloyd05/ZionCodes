dbb613f219b1f2215ee4d46f30d6f872ba803569
Alagbala Damilola <zionlloyd05@gmail.com>
2021-07-15 17:34:23 +0100

    ShouldLogWarningOnRetrieveAllWhenReadingNotesWasEmptyAndLogIt -> FAIL

---
+++ ZionCodes.Core.Tests.Unit/Services/ReadingNotes/ReadingNoteServiceTests.Validations.RetrieveAll.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Xunit;
+using ZionCodes.Core.Models.ReadingNotes;
+
+namespace ZionCodes.Core.Tests.Unit.Services.ReadingNotes
+{
+    public partial class ReadingNoteServiceTests
+    {
+        [Fact]
+        public void ShouldLogWarningOnRetrieveAllWhenReadingNotesWasEmptyAndLogIt()
+        {
+            // given
+            IQueryable<ReadingNote> emptyStorageReadingNotes = new List<ReadingNote>().AsQueryable();
+            IQueryable<ReadingNote> expectedReadingNotes = emptyStorageReadingNotes;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllReadingNotes())
+                    .Returns(expectedReadingNotes);
+
+            // when
+            IQueryable<ReadingNote> actualReadingNote =
+                this.readingNoteService.RetrieveAllReadingNotes();
+
+            // then
+            actualReadingNote.Should().BeEquivalentTo(expectedReadingNotes);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogWarning("No tags found in storage."));
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllReadingNotes(),
+                    Times.Once);
+
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
