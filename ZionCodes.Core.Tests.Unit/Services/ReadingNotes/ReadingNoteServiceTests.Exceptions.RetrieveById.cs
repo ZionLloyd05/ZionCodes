4538d16c663282c881c4a9c36bcd2eab7e20194c
Alagbala Damilola <zionlloyd05@gmail.com>
2021-07-15 20:10:55 +0100

    ShouldThrowDependencyExceptionOnRetrieveByIdWhenDbExceptionOccursAndLogItAsync -> PASS

--- ZionCodes.Core.Tests.Unit/Services/ReadingNotes/ReadingNoteServiceTests.Exceptions.RetrieveById.cs
+++ ZionCodes.Core.Tests.Unit/Services/ReadingNotes/ReadingNoteServiceTests.Exceptions.RetrieveById.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Xunit;
 using ZionCodes.Core.Models.ReadingNotes;
@@ -48,5 +49,41 @@ namespace ZionCodes.Core.Tests.Unit.Services.ReadingNotes
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRetrieveByIdWhenDbExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid someReadingNoteId = Guid.NewGuid();
+            var databaseUpdateException = new DbUpdateException();
+
+            var expectedReadingNoteDependencyException =
+                new ReadingNoteDependencyException(databaseUpdateException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectReadingNoteByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(databaseUpdateException);
+
+            // when
+            ValueTask<ReadingNote> retrieveByIdReadingNoteTask =
+                this.readingNoteService.RetrieveReadingNoteByIdAsync(someReadingNoteId);
+
+            // then
+            await Assert.ThrowsAsync<ReadingNoteDependencyException>(() =>
+                retrieveByIdReadingNoteTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedReadingNoteDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectReadingNoteByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
     }
 }
