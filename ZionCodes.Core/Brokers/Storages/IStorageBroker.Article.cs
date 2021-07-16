792d484ac861ab4dee02b424ffe044a7a94258f4
Alagbala Damilola <zionlloyd05@gmail.com>
2021-07-16 22:01:04 +0100

    Article model and migration set up completed

---
+++ ZionCodes.Core/Brokers/Storages/IStorageBroker.Article.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZionCodes.Core.Models.Articles;
+
+namespace ZionCodes.Core.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        public ValueTask<Article> InsertArticleAsync(Article article);
+        public IQueryable<Article> SelectAllArticles();
+        public ValueTask<Article> SelectArticleByIdAsync(Guid articleId);
+        public ValueTask<Article> UpdateArticleAsync(Article article);
+        public ValueTask<Article> DeleteArticleAsync(Article article);
+    }
+}
---
+++ ZionCodes.Core/Brokers/Storages/StorageBroker.Article.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using ZionCodes.Core.Models.Articles;
+
+namespace ZionCodes.Core.Brokers.Storages
+{
+    public partial class StorageBroker : IStorageBroker
+    {
+        public DbSet<Article> Articles { get; set; }
+
+        public async ValueTask<Article> InsertArticleAsync(Article article)
+        {
+            EntityEntry<Article> articleEntityEntry = await this.Articles.AddAsync(article);
+            await this.SaveChangesAsync();
+
+            return articleEntityEntry.Entity;
+        }
+
+        public IQueryable<Article> SelectAllArticles() => this.Articles.AsQueryable();
+
+        public async ValueTask<Article> SelectArticleByIdAsync(Guid articleId)
+        {
+            this.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+
+            return await Articles.FindAsync(articleId);
+        }
+
+        public async ValueTask<Article> UpdateArticleAsync(Article article)
+        {
+            EntityEntry<Article> articleEntityEntry = this.Articles.Update(article);
+            await this.SaveChangesAsync();
+
+            return articleEntityEntry.Entity;
+        }
+
+        public async ValueTask<Article> DeleteArticleAsync(Article article)
+        {
+            EntityEntry<Article> articleEntityEntry = this.Articles.Remove(article);
+            await this.SaveChangesAsync();
+
+            return articleEntityEntry.Entity;
+        }
+    }
+}
