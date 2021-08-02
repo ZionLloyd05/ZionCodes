367e54103250ac9fea9b349725a68b75313726e3
Alagbala Damilola <zionlloyd05@gmail.com>
2021-08-02 15:34:29 +0100

    Added article comment model and broker

---
+++ ZionCodes.Core/Brokers/Storages/IStorageBroker.ArticleComments.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ZionCodes.Core.Models.ArticleComments;
+
+namespace ZionCodes.Core.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        public ValueTask<ArticleComment> InsertArticleCommentAsync(ArticleComment ArticleComment);
+        public IQueryable<ArticleComment> SelectAllArticleComments();
+        public ValueTask<ArticleComment> SelectArticleCommentByIdAsync(Guid ArticleCommentId);
+        public ValueTask<ArticleComment> UpdateArticleCommentAsync(ArticleComment ArticleComment);
+        public ValueTask<ArticleComment> DeleteArticleCommentAsync(ArticleComment ArticleComment);
+    }
+}
---
+++ ZionCodes.Core/Models/ArticleComments/ArticleComment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ZionCodes.Core.Models.Articles;
+using ZionCodes.Core.Models.Comments;
+
+namespace ZionCodes.Core.Models.ArticleComments
+{
+    public class ArticleComment
+    {
+        public Guid ArticleId { get; set; }
+        public Article Article { get; set; }
+        public Guid CommentId { get; set; }
+        public Comment Comment { get; set; }
+    }
+}
